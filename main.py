6b172b087d96749dc594bbc7b8d2141472018ee0
Aarizali2 <161396725+Aarizali2@users.noreply.github.com>
2024-06-23 08:15:46 +0000

    cycling through the different player pngs

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class Quizstarter:
       self.parent = parent
       self.selected_option = IntVar()#selection option stored in an integer variable
       self.all_page_bg_image = "allpage.png"
-    #self.quiz_page_bg_image = 
       self.options_questions = [
       {
         "question_image":
@@ -27,11 +26,77 @@ class Quizstarter:
           "options": ["Ronaldo", "Ronaldino", "Mbappe", "Pogba"],
           "answer":
           3
-      }
+      },
+        {
+            "question_image":
+            "bale.png",
+            "options": ["David Luiz", "Gerard Piqué", "Gareth Bale", "Ben White"],
+            "answer":
+            3
+        },
+
+
+
+        {
+            "question_image":
+            "Iniesta.png",
+            "options": ["Gianluigi Buffon", "Andrés Iniesta", "Kevin De Bruyne", "Arjen Robben"],
+            "answer":
+            2
+        },
+
+
+
+        {
+            "question_image":
+            "Bastian.png",
+            "options": ["Mario Götze", " Bastian Schweinsteiger"
+, "Marco Reus", "Manuel Neuer"],
+            "answer":
+            2
+        },
+
+
+        {
+          "question_image":
+          "antoine.png",
+          "options": ["Antoine Griezmann", "Sergio Busquets " , "Alexis Sánchez", "Marco Reus"], 
+          "answer":
+          1
+        },
+
+
+        {
+          "question_image":
+          "Ozil.png",
+          "options": [" Iker Casillas"
+, "Sergio Agüero" , "Arjen Robben", "mesut özil"], 
+          "answer":
+          4
+        },
+
+        {
+          "question_image":
+          "blerim.png",
+          "options": ["Henrikh Mkhitaryan", "blerim džemaili" , "Yaya Touré", "Philippe Coutinho"], 
+          "answer":
+          2
+        },
+
+        {
+          "question_image":
+          "Emiliano.png",
+          "options": ["Emiliano Martínez", "Jan Oblak" , "Thibaut Courtois", "Franck Ribéry"], 
+          "answer":
+          1
+        },
+
+
                              ]
 
 
       self.current_question_index = 0
+      self.score = 0
       self.photoLabel = None
       self.photoLabel2 = None
       self.entry_box = None
@@ -44,8 +109,6 @@ class Quizstarter:
 
     #Button
       self.next_button = Button(parent, text="Next",  bg="#b8b4b4",command=self.show_questions)
-    
-                              
                              
       self.next_button.place(x=720, y=535, anchor="se")
 
@@ -69,7 +132,7 @@ class Quizstarter:
       self.all_page_image = ImageTk.PhotoImage(all_page_image)
       self.photoLabel2 = Label(self.parent, image=self.all_page_image)
       self.photoLabel2.place(x=0, y=0, relheight=1, relwidth=1)
-
+  
     
       self.show_next_questions()
     
@@ -77,16 +140,24 @@ class Quizstarter:
       if self.current_question_index < len(self.options_questions):
         question_data = self.options_questions[self.current_question_index]
  
-    
+        if self.question_image_label:
+          self.question_image_label.destroy()
+        for btn in self.options_buttons:
+          btn.destroy()
+          self.score_label.destroy()
+          self.feedback_label.destroy()
 
+     
+
+          
    #show question image 
 
       self.question_image_path = Image.open("imgs/" + question_data["question_image"])
       self.question_image = ImageTk.PhotoImage(self.question_image_path.resize((100, 100), Image.LANCZOS))
       self.question_image_label = Label(self.parent, image=self.question_image)
-      self.question_image_label.place(relx=0.4, rely=0.3)
+      self.question_image_label.place(relx=0.427, rely=0.2)
    
-  
+
 
       options = question_data["options"]
       self.options_buttons = []
@@ -107,31 +178,32 @@ class Quizstarter:
   
       self.feedback_label = Label(self.parent, fg="green", font=("Arial", 12))
       self.feedback_label.place(x=340, y=425)
-   # else:
-     # self.show_final_score()
+ 
+  
     #getting submit button to work
 
 
   def check_answer(self):
-    #the value 3 is for the correct answer
+    #checks through the dictionary for the correct answeer 
       question_data = self.options_questions[self.current_question_index]
-      correct_answer_value = question_data["answer"]
+      correct_answer = question_data["answer"]
       user_answer = self.selected_option.get()
+    
+#prints correct or incorerct depending on users answer
+      if user_answer == correct_answer:
+         self.feedback_label.config(text="Correct! You guessed right", fg="green")
+         self.score += 1
 
-      if user_answer == correct_answer_value:
-        self.feedback_label.config(text="Correct! You guessed right", fg="green")
-      #add 1 score every correct answer
-        self.score = 1
-        self.score_label.config(text=f"Score: {self.score}")
+        
+         self.score_label.config(text=f"score: {self.score}")
+         self.current_question_index += 1
       else:
         self.feedback_label.config(text="Incorrect! You guessed wrong", fg="red")
 
-      #this means it takes around 2 seconds to go to the next question page
-      self.parent.after(2000, self.options_questions)
 
- 
-    #question_number = question_number + 1
-    #destroy the first question page and make new one
+  
+      #this means it takes around 2 seconds to go to the next question page
+      self.parent.after(2000, self.show_next_questions)
     
 
 
